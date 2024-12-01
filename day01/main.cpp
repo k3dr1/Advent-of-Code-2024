38e4ada4d8db9a1996756effbfd61e3976fbfc2d
Ked <balkangrid@gmail.com>
2024-12-01 14:10:36 +0800

    Washed day1

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,10 +1,9 @@
-#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <string_view>
 #include <vector>
 #include <algorithm>
+#include <ranges>
 
 int main() {
 
@@ -12,21 +11,18 @@ int main() {
     auto answer2{0ll};
     auto f = std::fstream{"input.txt"};
     auto line = std::string{};
-    auto lines = std::vector<std::string>{};
-
-    while (std::getline(f, line)) lines.push_back(line);
-
     auto lefts = std::vector<long long>{};
     auto rights = std::vector<long long>{};
-    for (auto&& e : lines) {
-        lefts.push_back(std::stoi(e.substr(0, 6)));
-        rights.push_back(std::stoi(e.substr(6)));
+
+    while (std::getline(f, line)) {
+        lefts.push_back(std::stoi(line.substr(0, line.find(' '))));
+        rights.push_back(std::stoi(line.substr(line.find_last_of(' '))));
     }
 
     std::ranges::sort(lefts);
     std::ranges::sort(rights);
 
-    for (int i = 0; i < lefts.size(); i++) answer1 += std::abs(lefts.at(i) - rights.at(i));
+    for (auto&& [l,r] : std::views::zip(lefts, rights)) answer1 += std::abs(l - r);
     for (auto&& num : lefts) answer2 += std::ranges::count(rights, num) * num;
 
     std::cout << "part1=" << answer1 << '\n';
