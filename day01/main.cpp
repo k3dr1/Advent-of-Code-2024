e235fa5e49556502bb1d16a298d3bcdc6cffbb9c
Ked <balkangrid@gmail.com>
2024-12-01 18:10:04 +0800

    made it slightly faster by storing the frequency map

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,25 +1,30 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 #include <algorithm>
 #include <ranges>
+
+using ll = long long;
 
 int main() {
     auto answer1{0ll}, answer2{0ll};
     auto f = std::ifstream{"input.txt"};
     auto line = std::string{};
-    auto lefts = std::vector<long long>{};
-    auto rights = std::vector<long long>{};
+    auto lefts = std::vector<ll>{};
+    auto rights = std::vector<ll>{};
+    auto rights_freq = std::unordered_map<ll, ll>{};
     while (std::getline(f, line)) {
-        lefts.push_back(std::stoi(line.substr(0, line.find(' '))));
-        rights.push_back(std::stoi(line.substr(line.find_last_of(' '))));
+        lefts.emplace_back(std::stoi(line.substr(0, line.find(' '))));
+        rights.emplace_back(std::stoi(line.substr(line.find_last_of(' '))));
+        rights_freq[rights.back()]++;
     }
     std::ranges::sort(lefts);
     std::ranges::sort(rights);
     for (auto&& [l,r] : std::views::zip(lefts, rights)) {
         answer1 += std::abs(l - r);
-        answer2 += std::ranges::count(rights, l) * l;
+        answer2 += rights_freq[l] * l;
     }
     std::cout << "part1=" << answer1 << '\n';
     std::cout << "part2=" << answer2 << '\n';
