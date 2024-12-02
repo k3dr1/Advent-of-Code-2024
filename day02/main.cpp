03200c7452058e90890dccbdbacbbb79f1c9f6c2
Ked <balkangrid@gmail.com>
2024-12-02 23:20:32 +0800

    solved day02 completely and washed the code

--- day02/main.cpp
+++ day02/main.cpp
@@ -7,31 +7,34 @@
 #include <string>
 #include <vector>
 #include <ostream>
-
 using i64 = std::int64_t;
 
 auto is_safe(auto&& diffs) -> bool {
     return std::ranges::fold_left(diffs, true,
-               [](auto&& l, auto&& r) { return l && (0 < r && r < 4); })
+                   [](const auto& l, const auto& r) { return l && (0 < r && r < 4); })
         || std::ranges::fold_left(diffs, true,
-            [](auto&& l, auto&& r) { return l && (-4 < r && r < 0); });
+            [](const auto& l, const auto& r) { return l && (-4 < r && r < 0); });
 }
 
 auto is_safe2(auto&& diffs) -> bool
 {
     auto res { false };
-    for (const auto missing : std::views::iota(0uz, (size_t)std::ranges::distance(diffs))) {
+    for (const auto missing : std::views::iota(0z, std::ranges::distance(diffs))) {
         auto r = std::views::enumerate(diffs)
-            | std::views::drop_while(
-                [&missing](auto&& p) {
-                    auto&& [i, _] = p;
-                    return i == missing;
-                })
+            | std::views::filter([missing](const auto& p) -> bool {
+                  const auto& [i, v] = p;
+                  return { i != missing };
+              })
             | std::views::transform(
-                [](auto&& p) {
-                    auto&& [_, v] = p;
+                [](const auto& p) {
+                    const auto& [_, v] = p;
                     return v;
-                });
+                })
+            | std::views::adjacent<2>
+            | std::views::transform(
+                [](const auto& p) {
+                  const auto& [l, r] = p;
+                  return l - r; });
         res |= is_safe(r);
     }
     return res;
@@ -49,24 +52,18 @@ auto main() -> int {
     }
 
     for (auto line : lines) {
-        auto diffs = line
+        auto nums = line
             | std::views::split(' ')
-            | std::ranges::views::transform(
-                [](const auto& s) { return std::string(&*s.begin(), std::ranges::distance(s)); })
-            | std::views::transform(
-                [](const auto& e) { return std::atoll(e.c_str()); })
+            | std::ranges::views::transform( [](const auto& s) { return std::string(&*s.begin(), std::ranges::distance(s)); })
+            | std::views::transform( [](const auto& e) { return std::atoll(e.c_str()); });
+        auto diffs = nums 
             | std::views::adjacent<2>
             | std::views::transform(
                 [](const auto& p) {
                   const auto& [l, r] = p;
                   return l - r; });
-        // 1 2 6 7 8
-        //  1 4 1 1
-        //
-        // 0 1 5 6
-        //  1 4 1
         answer1 += is_safe(diffs);
-        answer2 += is_safe2(diffs);
+        answer2 += is_safe2(nums);
     }
 
     std::cout << "Answer1: " << answer1 << '\n';
