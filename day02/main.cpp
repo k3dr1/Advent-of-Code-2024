a28b59ee55c05e155e6cb14e512a1fb958f1f3f0
Ked <balkangrid@gmail.com>
2024-12-02 23:55:24 +0800

    washed the code

--- day02/main.cpp
+++ day02/main.cpp
@@ -7,67 +7,40 @@
 #include <string>
 #include <vector>
 #include <ostream>
+
 using i64 = std::int64_t;
 
+auto tuple_difference = [](auto&& p) { const auto& [l, r] = p; return l - r; };
 auto is_safe(auto&& diffs) -> bool {
-    return std::ranges::fold_left(diffs, true,
-                   [](const auto& l, const auto& r) { return l && (0 < r && r < 4); })
-        || std::ranges::fold_left(diffs, true,
-            [](const auto& l, const auto& r) { return l && (-4 < r && r < 0); });
+    return std::ranges::fold_left(diffs, true, [](const auto& l, const auto& r) { return l && (0 < r && r < 4); })
+        || std::ranges::fold_left(diffs, true, [](const auto& l, const auto& r) { return l && (-4 < r && r < 0); });
 }
-
-auto is_safe2(auto&& diffs) -> bool
-{
+auto is_safe2(auto&& diffs) -> bool {
     auto res { false };
     for (const auto missing : std::views::iota(0z, std::ranges::distance(diffs))) {
-        auto r = std::views::enumerate(diffs)
-            | std::views::filter([missing](const auto& p) -> bool {
-                  const auto& [i, v] = p;
-                  return { i != missing };
-              })
-            | std::views::transform(
-                [](const auto& p) {
-                    const auto& [_, v] = p;
-                    return v;
-                })
+        res |= is_safe(std::views::enumerate(diffs)
+            | std::views::filter([missing](const auto& p) -> bool { const auto& [i, v] = p; return { i != missing }; })
+            | std::views::transform([](const auto& p) { const auto& [_, v] = p; return v; })
             | std::views::adjacent<2>
-            | std::views::transform(
-                [](const auto& p) {
-                  const auto& [l, r] = p;
-                  return l - r; });
-        res |= is_safe(r);
+            | std::views::transform(tuple_difference));
     }
     return res;
 }
-
 auto main() -> int {
     i64 answer1{0};
     i64 answer2{0};
     auto f = std::fstream{"input.txt"};
     std::string line{};
     std::vector<std::string> lines{};
-
     while (std::getline(f, line)) {
-        lines.emplace_back(line);
-    }
-
-    for (auto line : lines) {
-        auto nums = line
-            | std::views::split(' ')
-            | std::ranges::views::transform( [](const auto& s) { return std::string(&*s.begin(), std::ranges::distance(s)); })
+        auto nums = line | std::views::split(' ')
+            | std::views::transform( [](const auto& s) { return std::string(&*s.begin(), std::ranges::distance(s)); })
             | std::views::transform( [](const auto& e) { return std::atoll(e.c_str()); });
-        auto diffs = nums 
-            | std::views::adjacent<2>
-            | std::views::transform(
-                [](const auto& p) {
-                  const auto& [l, r] = p;
-                  return l - r; });
+        auto diffs = nums | std::views::adjacent<2> | std::views::transform(tuple_difference);
         answer1 += is_safe(diffs);
         answer2 += is_safe2(nums);
     }
-
     std::cout << "Answer1: " << answer1 << '\n';
     std::cout << "Answer2: " << answer2 << '\n';
-
     return 0;
 }
