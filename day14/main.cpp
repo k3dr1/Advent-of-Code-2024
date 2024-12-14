d10d503957db9aae4a81f8db66407fe4ec3b30b6
Ked <balkangrid@gmail.com>
2024-12-15 03:22:40 +0800

    updated

--- day14/main.cpp
+++ day14/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdint>
 #include <fstream>
 #include <print>
@@ -5,6 +6,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <execution>
 
 using ll = std::int64_t;
 
@@ -28,13 +30,11 @@ int main() {
                                 std::stoll(line.substr(line.find('=') + 1)));
         auto v = std::make_pair(std::stoll(line.substr(line.find_last_of(',') + 1)),
                                 std::stoll(line.substr(line.find_last_of('=') + 1)));
-        std::println("p = {} {}; v = {} {}", p.first, p.second, v.first, v.second);
         for (ll i = 0; i < 100; i++) {
             p = add(p,v);
             p.first = ((p.first+HEIGHT)%HEIGHT);
             p.second = ((p.second+WIDTH)%WIDTH);
         }
-        std::println("trying to place at {} {}", p.first, p.second);
         grid.at(p.first).at(p.second)++;
     }
 
@@ -47,7 +47,6 @@ int main() {
             constexpr auto midheight = ((HEIGHT-1)/2);
             constexpr auto midwidth = ((WIDTH-1)/2);
             if ((y == midheight) || (x == midwidth)) {
-                std::print(" ");
                 continue;
             }
             if (y < midheight) {
@@ -64,37 +63,39 @@ int main() {
                 }
             }
         }
-        std::print("\n");
     }
 
     std::println("Ans1: {}",topleft*topright*botleft*botright);
 
     auto grid2 = std::vector<std::vector<char>>(HEIGHT, std::vector<char>(WIDTH, '0'));
-
-    for (ll i = 0; i < 10'000; i++) {
-        clear_grid(grid2);
-        for (const auto& line : lines) {
-            auto p = std::make_pair(std::stoll(line.substr(line.find(',') + 1)),
-                                    std::stoll(line.substr(line.find('=') + 1)));
-            auto v = std::make_pair(std::stoll(line.substr(line.find_last_of(',') + 1)),
-                                    std::stoll(line.substr(line.find_last_of('=') + 1)));
-            for (ll j = 0; j < i; j++) {
-                p = add(p,v);
-                p.first = ((p.first+HEIGHT)%HEIGHT);
-                p.second = ((p.second+WIDTH)%WIDTH);
+    auto iterations = std::views::iota(0, 10'000);
+    std::for_each(
+        std::execution::par_unseq,
+        iterations.begin(),
+        iterations.end(),
+        [&](const auto i) {
+            clear_grid(grid2);
+            for (const auto& line : lines) {
+                auto p = std::make_pair(std::stoll(line.substr(line.find(',') + 1)),
+                    std::stoll(line.substr(line.find('=') + 1)));
+                auto v = std::make_pair(std::stoll(line.substr(line.find_last_of(',') + 1)),
+                    std::stoll(line.substr(line.find_last_of('=') + 1)));
+                for (ll j = 0; j < i; j++) {
+                    p = add(p, v);
+                    p.first = ((p.first + HEIGHT) % HEIGHT);
+                    p.second = ((p.second + WIDTH) % WIDTH);
+                }
+                grid2.at(p.first).at(p.second)++;
             }
-            grid2.at(p.first).at(p.second)++;
-        }
 
-        bool no_robot_occupies_the_same_spot{true};
-        for (auto& row : grid2)
-            for (auto& val : row)
-                no_robot_occupies_the_same_spot &= ((val == '0') || (val == '1'));
-        if (no_robot_occupies_the_same_spot) {
-            std::println("Ans2: {}", i);
-            break;
-        }
-    }
+            bool no_robot_occupies_the_same_spot { true };
+            for (auto& row : grid2)
+                for (auto& val : row)
+                    no_robot_occupies_the_same_spot &= ((val == '0') || (val == '1'));
+            if (no_robot_occupies_the_same_spot) {
+                std::println("Ans2: {}", i);
+            }
+        });
 
     return 0;
 }
