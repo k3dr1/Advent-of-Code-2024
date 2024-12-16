eba0c5ca0a70277af1391b15f7d560c2e84a33d1
Ked <balkangrid@gmail.com>
2024-12-17 00:37:38 +0800

    kinda solved part2

--- day16/main.cpp
+++ day16/main.cpp
@@ -1,7 +1,6 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <ostream>
 #include <print>
 #include <queue>
 #include <string>
@@ -67,7 +66,7 @@ struct hasher {
     }
 };
 
-auto solve_part1(const std::vector<std::vector<char>>& grid) {
+auto solve_part1(const std::vector<std::vector<char>>& grid) -> ll {
     auto start = gridfind('S', grid);
     auto end = gridfind('E', grid);
     auto comp = [](const node& l, const node& r) { return l.distance > r.distance; };
@@ -107,9 +106,117 @@ auto solve_part1(const std::vector<std::vector<char>>& grid) {
     std::unreachable();
 }
 
+auto dijkstra(vec2 start, ll start_dir, vec2 end, const std::vector<std::vector<char>>& grid, std::unordered_map<std::pair<vec2, ll>, ll, hasher> visited) {
+    auto comp = [](const node& l, const node& r) { return l.distance > r.distance; };
+
+    std::priority_queue<node, 
+                        std::vector<node>, 
+                        decltype(comp)> pq {comp};
+    // [pos, dir] -> [dist]
+    //std::unordered_map<std::pair<vec2, ll>, ll, hasher> visited{};
+
+    pq.push({start, 0, start_dir});
+    while (!pq.empty()) {
+        auto [pos, dist, dir] = pq.top();
+        pq.pop();
+        if (pos == end) return dist;
+        visited[std::make_pair(pos, dir)] = dist;
+        // Advance
+        auto next_pos = pos + directions[dir];
+        if (grid.at(next_pos.y).at(next_pos.x) != '#') {
+            if (!visited.contains(std::make_pair(next_pos, dir)) || visited.at(std::make_pair(next_pos, dir)) > (dist+1)) {
+                pq.push(node{next_pos,dist+1,dir});
+            } 
+        }
+
+        // Clockwise
+        auto cw_dir = (dir+1)%4;
+        if (!visited.contains(std::make_pair(pos, cw_dir)) || visited.at(std::make_pair(pos, cw_dir)) > (dist)) {
+            pq.push(node { pos, dist + 1000, cw_dir });
+        } 
+
+        // Counter-clockwise
+        auto ccw_dir = (dir+3)%4;
+        if (!visited.contains(std::make_pair(pos, ccw_dir)) || visited.at(std::make_pair(pos, ccw_dir)) > (dist)) {
+            pq.push(node { pos, dist + 1000, ccw_dir });
+        } 
+    }
+    std::unreachable();
+}
+
+struct vhash {
+    bool operator()(const vec2& v) const {
+        return (v.x<<32) ^ (v.y); 
+    }
+};
+
+auto solve_part2_checking_each_node(const std::vector<std::vector<char>>& grid) -> ll {
+    auto start = gridfind('S', grid);
+    auto end = gridfind('E', grid);
+    auto comp = [](const node& l, const node& r) { return l.distance > r.distance; };
+
+    std::priority_queue<node, 
+                        std::vector<node>, 
+                        decltype(comp)> pq {comp};
+    // [pos, dir] -> [dist]
+    std::unordered_map<std::pair<vec2, ll>, ll, hasher> visited{};
+
+    auto shortest_distance{0};
+    pq.push({start, 0, 1});
+    while (!pq.empty()) {
+        auto [pos, dist, dir] = pq.top();
+        pq.pop();
+        visited[std::make_pair(pos, dir)] = dist;
+        if (pos == end) {
+            shortest_distance = dist;
+            break;
+        }
+        // Advance
+        auto next_pos = pos + directions[dir];
+        if (grid.at(next_pos.y).at(next_pos.x) != '#') {
+            if (!visited.contains(std::make_pair(next_pos, dir)) || visited.at(std::make_pair(next_pos, dir)) > (dist+1)) {
+                pq.push(node{next_pos,dist+1,dir});
+            } 
+        }
+
+        // Clockwise
+        auto cw_dir = (dir+1)%4;
+        if (!visited.contains(std::make_pair(pos, cw_dir)) || visited.at(std::make_pair(pos, cw_dir)) > (dist)) {
+            pq.push(node { pos, dist + 1000, cw_dir });
+        } 
+
+        // Counter-clockwise
+        auto ccw_dir = (dir+3)%4;
+        if (!visited.contains(std::make_pair(pos, ccw_dir)) || visited.at(std::make_pair(pos, ccw_dir)) > (dist)) {
+            pq.push(node { pos, dist + 1000, ccw_dir });
+        } 
+    }
+
+    std::println("Shortest_path is {}", shortest_distance);
+    auto num_optimal_cells{0};
+    std::unordered_map<vec2, ll, vhash> optimal_cells{};
+    auto counter{0};
+    for (auto& cell : visited) {
+
+        auto [pos_dir, dist_from_start] = cell;
+        auto dist_to_end = dijkstra(pos_dir.first, pos_dir.second, end, grid, {});
+
+        //std::println("Checking node {},{}", pos_dir.first.x, pos_dir.first.y);
+        //std::println("from_start: {}", dist_from_start);
+        //std::println("calculated dist_to_end: {}", dist_to_end);
+
+        if ((dist_from_start + dist_to_end) == shortest_distance) {
+            optimal_cells[pos_dir.first]++;
+        }
+        counter++;
+    }
+
+    return optimal_cells.size();
+}
+
 int main() {
 
-    auto f = std::ifstream("input.txt");
+    auto f = std::ifstream("example.txt");
     auto grid = std::vector<std::vector<char>>{};
 
     for (std::string line {}; std::getline(f, line);) {
@@ -119,7 +226,12 @@ int main() {
 
     printgrid(grid);
 
-    std::println("Ans1: {}", solve_part1(grid));
+    auto ans1 = solve_part1(grid);
+    auto ans2 = solve_part2_checking_each_node(grid);
+
+    // not 480, but it's 481 for some reason :skull:
+    std::println("Ans1: {}", ans1);
+    std::println("Ans2: {}", ans2);
 
     return 0;
 }
